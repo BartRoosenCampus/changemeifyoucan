bae015819fcf542f12b173a0034570c793858db9
Bart <bart.roosen@vdabcampus.be>
2022-02-10 17:07:49 +0100

    20220210: test

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+// index.php
+declare(strict_types = 1);
+
+$content = 'hello world';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <title>$Title$</title>
+</head>
+<body>
+<h1><?= $content; ?></h1>
+</body>
+</html>
